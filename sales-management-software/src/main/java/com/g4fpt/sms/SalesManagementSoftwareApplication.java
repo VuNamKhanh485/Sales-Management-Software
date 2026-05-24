281c54aacd02f32455f411e902755cf8538989e9
VuNamKhanh485 <khanhtit16@gmail.com>
2026-05-24 12:39:53 +0700

    update report, project base

---
+++ sales-management-software/src/main/java/com/g4fpt/sms/SalesManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.g4fpt.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesManagementSoftwareApplication.class, args);
+    }
+
+}
---
+++ sales-management-software/src/test/java/com/g4fpt/sms/SalesManagementSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g4fpt.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagementSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
