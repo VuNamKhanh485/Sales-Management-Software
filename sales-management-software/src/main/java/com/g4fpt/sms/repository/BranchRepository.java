f7e32fbaddee1cb9906035da86e0bc9e495ea370
nguyenvuhung <nguyenvuhung.12a7@gmail.com>
2026-05-27 23:40:29 +0700

    Add branch management feature

---
+++ sales-management-software/src/main/java/com/g4fpt/sms/repository/BranchRepository.java
@@ -0,0 +1,7 @@
+package com.g4fpt.sms.repository;
+
+import com.g4fpt.sms.entity.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchRepository extends JpaRepository<Branch, Long> {
+}
