64df1588869ba1598f02fd318055d4875a74ec02
VuNamKhanh485 <khanhtit16@gmail.com>
2026-05-31 23:06:52 +0700

    add service

--- sales-management-software/src/main/java/com/g4fpt/sms/product/repository/CategoryRepository.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/product/repository/CategoryRepository.java
@@ -1,8 +1,10 @@
 package com.g4fpt.sms.product.repository;
 
+import com.g4fpt.sms.product.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
