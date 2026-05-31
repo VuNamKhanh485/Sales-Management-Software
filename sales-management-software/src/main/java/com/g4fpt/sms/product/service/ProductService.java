36f56e0078c4a8bf92d98815eb3f973f897351dc
VuNamKhanh485 <khanhtit16@gmail.com>
2026-06-01 00:00:06 +0700

    update service

--- sales-management-software/src/main/java/com/g4fpt/sms/product/service/ProductService.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/product/service/ProductService.java
@@ -1,4 +1,15 @@
 package com.g4fpt.sms.product.service;
 
+import com.g4fpt.sms.product.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    Product create(Product product);
+    Product update(Product product);
+    Product findById(long id);
+    Product findByName(String name);
+    Product findByBrand(String brand);
+    void delete(long id);
+    List<Product> getAll();
 }
