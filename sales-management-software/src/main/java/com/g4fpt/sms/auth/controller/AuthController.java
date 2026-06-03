bf67366a24b3fead8ffca7e1d5e9ddb62a0e5bc4
xhuyvu <xhuyvu.eng@gmail.com>
2026-06-04 01:36:15 +0700

    changing

--- sales-management-software/src/main/java/com/g4fpt/sms/auth/controller/AuthController.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/auth/controller/AuthController.java
@@ -11,29 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class AuthController {
 
-    private final AuthService authService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
-
-    @GetMapping("/login")
-    public String loginPage() {
-        return "auth/login";
-    }
-
-    @PostMapping("/login")
-    public String login(@ModelAttribute LoginRequest loginRequest, HttpSession session, RedirectAttributes redirectAttributes) {
-        try {
-            CustomUserDetails userDetails = authService.login(loginRequest);
-            session.setAttribute("loggedInUser", userDetails);
-            redirectAttributes.addFlashAttribute("loginSuccess", "Đăng nhập thành công.");
-            return "redirect:/login";
-        } catch (Exception exception) {
-            redirectAttributes.addFlashAttribute("loginError", "Email hoặc mật khẩu không đúng.");
-            return "redirect:/login";
-        }
-    }
 
 
 }
