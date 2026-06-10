02c1837b1404d657062ad879fade7d7e137c35f4
VuNamKhanh485 <khanhtit16@gmail.com>
2026-06-10 16:55:48 +0700

    add validate

--- sales-management-software/src/main/java/com/g4fpt/sms/product/dto/request/CategoryRequest.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/product/dto/request/CategoryRequest.java
@@ -2,16 +2,17 @@ package com.g4fpt.sms.product.dto.request;
 
 import com.g4fpt.sms.product.enums.CategoryStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CategoryRequest {
-
+    @NotBlank(message = "Name is required")
     private String categoryName;
 
     private String description;
-
+    @NotNull(message = "Status is required")
     private CategoryStatus categoryStatus;
 }
--- sales-management-software/src/main/java/com/g4fpt/sms/product/dto/request/ProductRequest.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/product/dto/request/ProductRequest.java
@@ -3,6 +3,8 @@ package com.g4fpt.sms.product.dto.request;
 import com.g4fpt.sms.product.entity.Brand;
 import com.g4fpt.sms.product.entity.Category;
 import com.g4fpt.sms.product.enums.ProductStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +14,12 @@ import java.util.List;
 @Getter
 @Setter
 public class ProductRequest {
+    @NotBlank(message = "Category is required")
     private Long categoryId;
+    @NotBlank(message = "Brand is required")
     private Long brandId;
+    @NotBlank(message = "Name is required")
+    @Size(message = "Name must be at least 3 chars")
     private String name;
     private String imageUrl;
     private String description;
