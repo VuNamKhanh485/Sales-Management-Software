a1017a565bab3f984a649fadf3c679c627b409af
VuNamKhanh485 <227631105+VuNamKhanh485@users.noreply.github.com>
2026-06-24 23:49:18 +0700

    service delete

--- sales-management-software/src/main/java/com/g4fpt/sms/product/repository/ProductRepository.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/product/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.g4fpt.sms.product.repository;
 
 import com.g4fpt.sms.product.entity.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -16,11 +18,13 @@ public interface ProductRepository extends JpaRepository<Product, Long>,
     List<Product> findByCategory_Id(Long categoryId);
     List<Product> findByNameContainingIgnoreCase(String name);
     boolean existsByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCaseAndIdNot(String name, Long excludeId);
     @Query("""
             SELECT CASE WHEN COUNT(otd) > 0 THEN true ELSE false END
             FROM OrderTransactionDetail otd
             WHERE otd.productUnit.product.id =: productId
     """)
     boolean existInOrderTransaction(@Param("productId") Long productId);
+
 
 }
