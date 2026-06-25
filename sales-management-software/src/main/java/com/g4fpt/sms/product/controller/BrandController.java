04f7275a3caec87a4da0ca3c943ef89aed6026b9
VuNamKhanh485 <227631105+VuNamKhanh485@users.noreply.github.com>
2026-06-25 18:26:08 +0700

    fix find all

--- sales-management-software/src/main/java/com/g4fpt/sms/product/controller/BrandController.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/product/controller/BrandController.java
@@ -33,7 +33,7 @@ public class BrandController {
                        @RequestParam(defaultValue = "10") int size,
                        @RequestParam(defaultValue = "name") String sortField,
                        @RequestParam(defaultValue = "asc") String sortDir) {
-        Page<BrandResponse> brandPage = brandService.findAll(keyword, size, page, sortField, sortDir);
+        Page<BrandResponse> brandPage = brandService.findAll(keyword, page, size, sortField, sortDir);
 
         model.addAttribute("brandPage", brandPage);
         model.addAttribute("keyword", keyword);
