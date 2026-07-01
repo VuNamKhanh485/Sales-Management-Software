d0664c2d7db5dee9c3f7bd7eccba45dde2780897
VuNamKhanh485 <227631105+VuNamKhanh485@users.noreply.github.com>
2026-07-01 23:59:16 +0700

    Revert "revert: restore original POS order code from feature/order branch and remove Hungs import/order files"
    This reverts commit 0ef14e9d1519e7811d52181ec51f9d7be4b29f26.

--- sales-management-software/src/main/java/com/g4fpt/sms/order/entity/OrderTransactionDetail.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/order/entity/OrderTransactionDetail.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "OrderTransactionDetail")
+@Table(name = "ordertransactiondetail")
 @Getter
 @Setter
 @NoArgsConstructor
