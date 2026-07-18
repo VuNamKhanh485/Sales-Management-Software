40cadf27366b1b3d93e083bc15a0aab4199a3531
VuNamKhanh485 <227631105+VuNamKhanh485@users.noreply.github.com>
2026-07-18 12:14:06 +0700

    base sku sort

--- sales-management-software/src/main/java/com/g4fpt/sms/product/service/impl/ProductServiceImpl.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/product/service/impl/ProductServiceImpl.java
@@ -135,15 +135,27 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Page<ProductResponse> findAll(ProductFilterRequest filter, int page, int size,
                                          String sortField, String sortDir) {
-        Sort sort = sortDir.equalsIgnoreCase("asc")
-                ? Sort.by(sortField).ascending()
-                : Sort.by(sortField).descending();
+        if ("baseSku".equals(sortField)) {
+            Pageable pageable = PageRequest.of(page, size);
+            if(sortDir.equals("asc")) {
+                return productRepository.findAllOrderByBaseSkuAsc(filter, pageable)
+                        .map(productMapper::toResponse);
+            }
+            else{
+                return productRepository.findAllOrderByBaseSkuDesc(filter, pageable)
+                        .map(productMapper::toResponse);
+            }
+        } else {
+            Sort sort = sortDir.equalsIgnoreCase("asc")
+                    ? Sort.by(sortField).ascending()
+                    : Sort.by(sortField).descending();
 
-        Pageable pageable = PageRequest.of(page, size, sort);
-        Specification<Product> spec = ProductSpecification.fromFilter(filter);
+            Pageable pageable = PageRequest.of(page, size, sort);
+            Specification<Product> spec = ProductSpecification.fromFilter(filter);
 
-        return productRepository.findAll(spec, pageable)
-                .map(productMapper::toResponse);
+            return productRepository.findAll(spec, pageable)
+                    .map(productMapper::toResponse);
+        }
     }
 
     private Product getProductById(long id) {
