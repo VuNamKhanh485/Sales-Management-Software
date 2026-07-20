68041a304210468d2a8b7b3a98ccabdb00902933
VuNamKhanh485 <227631105+VuNamKhanh485@users.noreply.github.com>
2026-07-20 20:59:27 +0700

    update ignor

--- sales-management-software/src/main/java/com/g4fpt/sms/auth/service/AuthService.java
+++ sales-management-software/src/main/java/com/g4fpt/sms/auth/service/AuthService.java
@@ -1,9 +1,6 @@
 package com.g4fpt.sms.auth.service;
 
 import com.g4fpt.sms.auth.dto.SessionUser;
-import com.g4fpt.sms.auth.security.PasswordUtil;
-import com.g4fpt.sms.employee.repository.EmployeeRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
